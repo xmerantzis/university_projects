9a476cc40f8572c165b295f69e0b4dda13657a05
xmerantzis <xmerantzis@gmail.com>
2024-02-27 00:33:09 +0100

    Create index.php

---
+++ 3. web_project/web2022code/index.php
@@ -0,0 +1,16 @@
+<?php include 'header.php' ?>
+
+ <div class="scrollmenu">
+    <a href="index.php">Home</a>
+    <a href="Visualization.php">Καταστήματα-POI</a>
+    <a class="active" href="Declare_visit.php">Δήλωση Επίσκεψης</a>
+    <a href="covid_visit.php">Πιθανή Επαφή με Κρούσμα</a>
+    <a href="Declare_Covid.php">Δήλωση Κρούσματος</a>
+    <a href="Profile Management.php">Προφίλ</a>
+    <a href="logout.php">Logout</a>
+  </div>
+
+<title>This is the Main page</title>
+<h1 class="Welcome" style="color:white";>Welcome User</h1>
+<div class="Background">
+</div>
