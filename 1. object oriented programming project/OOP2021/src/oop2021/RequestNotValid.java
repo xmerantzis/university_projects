b074e65a3e8effe56b7dc87723a90ae8c09f8422
xmerantzis <xmerantzis@gmail.com>
2024-02-27 00:28:10 +0100

    Create RequestNotValid.java

---
+++ 1. object oriented programming project/OOP2021/src/oop2021/RequestNotValid.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop2021;
+
+/**
+ *
+ * @author spiro
+ */
+public class RequestNotValid extends Exception{
+    
+    public RequestNotValid(String message){
+        super(message);
+    }
+    
+}
