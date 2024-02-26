672c96c8260fb46d3023d68e2a77add22aea0c65
xmerantzis <xmerantzis@gmail.com>
2024-02-27 00:27:46 +0100

    Create EntityExists.java

---
+++ 1. object oriented programming project/OOP2021/src/oop2021/EntityExists.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop2021;
+
+/**
+ *
+ * @author spiro
+ */
+public class EntityExists extends Exception{
+    public EntityExists(){
+        super("Entity exists");
+    }
+}
