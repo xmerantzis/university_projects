f00764c3f00dff55d3849a77351af9fa7160a8bf
xmerantzis <xmerantzis@gmail.com>
2024-02-27 00:28:21 +0100

    Create UserType.java

---
+++ 1. object oriented programming project/OOP2021/src/oop2021/UserType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop2021;
+
+/**
+ *
+ * @author spiro
+ */
+public enum UserType {
+    ADMIN, BENEFICIARY, DONATOR
+}
